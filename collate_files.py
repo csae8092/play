2778fca4e3b922942c94985e9a2e188d728fa5f5
csae8092 <peter.andorfer@oeaw.ac.at>
2022-04-13 15:15:14 +0200

    merge collation results

--- collate_files.py
+++ collate_files.py
@@ -1,9 +1,23 @@
+import glob
+import lxml.etree as ET
+
 from acdh_collatex_utils.acdh_collatex_utils import CxCollate
+from acdh_collatex_utils.post_process import merge_tei_fragments
+
+input_glob = "./werke/uber-den-traum/uber-den-traum__*.xml"
+output_dir = "./werke/uber-den-traum/collated"
+werk_path = "uber-den-traum"
 
 out = CxCollate(
-    glob_pattern="./werke/uber-den-traum/uber-den-traum__*.xml",
+    glob_pattern=input_glob,
     glob_recursive=False,
-    output_dir="./werke/uber-den-traum/collated",
+    output_dir=output_dir,
     char_limit=False,
-    chunk_size=10000,
+    chunk_size=5000,
 ).collate()
+
+files = glob.glob(f"{output_dir}/*.xml")
+full_doc = merge_tei_fragments(files)
+
+with open(f'{werk_path}.xml', 'w') as f:
+    f.write(ET.tostring(full_doc, encoding='UTF-8').decode('utf-8'))
